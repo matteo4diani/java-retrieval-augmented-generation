6af849e952b30fa72ba05109971be4bce347e585
Matteo Fordiani <matteofordiani@gmail.com>
2024-04-09 13:30:32 +0200

    feat: add Spring Shell

---
+++ src/main/java/dev/sashacorp/javarag/JavaragApplication.java
@@ -0,0 +1,13 @@
+package dev.sashacorp.javarag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaragApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaragApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sashacorp/javarag/JavaragApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sashacorp.javarag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaragApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
